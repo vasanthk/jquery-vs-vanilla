ca6f8119f0c114d67b2c73e5b2957b9e51f5e41b
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-10-28 15:28:08 -0700

    getJSON

---
+++ getJSON.js
@@ -0,0 +1,28 @@
+/**
+ * Get JSON
+ */
+
+(function ($) {
+  // jQuery
+  $.getJSON('url/path', function (data) {
+    // Code to handle data
+  });
+
+  // Vanilla JS
+  var request = new XMLHttpRequest();
+  // Signature open(method, url, async)
+  request.open('GET', 'url/path', true);
+
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      // Success
+      var data = JSON.parse(request.responseText);
+    } else {
+      console.log('We reached our target server, but it returned an error.');
+    }
+  };
+
+  request.onerror = function () {
+    // There was a connection error of some sort.
+  };
+})(jQuery);
