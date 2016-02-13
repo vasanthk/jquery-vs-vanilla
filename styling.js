51c25016a3e2464d6916f52205b990f2ea862480
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-02-13 11:31:25 -0800

    CSS box model properties

--- styling.js
+++ styling.js
@@ -41,3 +41,44 @@ alert(div.style.cssText);
 var computedStyle = getComputedStyle(document.body, null);
 alert(computedStyle.marginTop);
 
+// CSS Box Model
+
+// clientWidth/Height
+// Size of the client area: content area with paddings, but without scrollbars.
+
+// scrollWidth/Height
+// Content area width and height including the scrolled out part.
+// scrollWidth/Height is same as clientWidth/Height, but includes full scrollable area.
+element.style.height = element.scrollHeight + 'px';
+
+// scrollTop/scrollLeft
+// Size of scrolled out part: vertical and horizontal. The value is always in pixels.
+// scrollLeft/scrollTop are writeable
+// Unlike other properties, which are read-only, you can change scrollLeft/scrollTop, and the browser scrolls the element.
+// In standards mode, the scroll of the document is in document.documentElement.
+// The following code scrolls the document 10px down:
+document.documentElement.scrollTop += 10;
+
+//offsetWidth/Height
+//Outer box width/height, full size with borders, but without margins.
+
+//clientTop/Left
+//The indent of client area from box outer corner. In other words, the width of top/left border in pixels.
+
+//offsetParent, offsetLeft/Top
+//Properties offsetLeft and offsetTop reflect a relative shift of an element from its offsetParent.
+//The offsetParent is the parent element in the sense of layout. For example, if an element is positioned absolutely, the offsetParent is not it’s DOM parent, but a nearest positioned element (or BODY).
+// We could use this to check if an elem is hidden:
+function isHidden(elem) {
+  return !elem.offsetWidth && !elem.offsetHeight;
+}
+// SUMMARY
+// Link: http://javascript.info/files/tutorial/browser/dom/metricSummary.png
+//
+//clientWidth/clientHeight - width/height of the visible in-border area (can be called a client area. The client area includes padding and doesn’t include scrollbars.
+//clientLeft/clientTop - left/top border width or, more generally, a shift of the client area from the top-left corner of the box.
+//scrollWidth/scrollHeight - width/height of the scrollable in-border area. Includes padding. Doesn’t include scrollbars.
+//scrollLeft/scrollTop - the width/height of the scrolled out part of the document, starting from the top-left corner.
+//offsetWidth/offsetHeight - the “outer” width/height of the box as seen from outside, excluding margins.
+//offsetParent - the nearest table-cell, body for static positioning or the nearest positioned element for other positioning types.
+//offsetLeft/offsetTop - the position in pixels of top-left corner of the box related to it’s offsetParent.
